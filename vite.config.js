bbe693fc01aa3b1b7b24de44161ae41eb450e725
Kamlesh Choudhary <kamleshchoudhary4846@gmail.com>
2025-12-27 11:49:55 +0530

    Clean docs build using emptyOutDir

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Portfolio-R/',
+   build: {
+    outDir: "docs",
+    emptyOutDir: true
+  },
 })
 
